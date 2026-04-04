b40dd22cf6fca3dc5c56b7f62f64ec040abb0c7d
iamblackdev <wahabhabib23@gmail.com>
2026-04-04 13:13:45 +0100

    refactor: app header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,16 @@
 import styles from './Header.module.css';
 
 export default function Header() {
-  return (
-    <header className={styles.header}>
-      <div className={styles.inner}>
-        <div className={styles.logo}>
-          <span className={styles.icon} aria-hidden="true">✦</span>
-          <span className={styles.title}>Star Wars Explorer</span>
-        </div>
-        <p className={styles.subtitle}>Galactic database powered by SWAPI</p>
-      </div>
-    </header>
-  );
+	return (
+		<header className={styles.header}>
+			<div className={styles.inner}>
+				<div className={styles.logo}>
+					<span className={styles.icon} aria-hidden="true">
+						✦
+					</span>
+					<span className={styles.title}>Star Wars Explorer</span>
+				</div>
+			</div>
+		</header>
+	);
 }
